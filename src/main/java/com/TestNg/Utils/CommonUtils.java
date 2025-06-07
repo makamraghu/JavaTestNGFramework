5b5a3ae2aaeb78f692d02a1facb262e39874bbee
raghuvardhan <raghuvardhan@macbookpro.lan>
2025-06-06 23:31:25 -0500

    locators page

--- src/main/java/com/TestNg/Utils/CommonUtils.java
+++ src/main/java/com/TestNg/Utils/CommonUtils.java
@@ -1,5 +1,16 @@
 package com.TestNg.Utils;
 
-public class CommonUtils {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class CommonUtils {
+	private WebDriver driver;
+
+    public CommonUtils(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By userName = By.id("username");
+    private By password = By.id("password");
+    private By loginBtn = By.id("login");
 }
